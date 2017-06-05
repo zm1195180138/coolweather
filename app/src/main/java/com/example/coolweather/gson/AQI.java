061e3aa277af3c78c3a97287e3c8b96f3adfd9f0
zm <1195180138@qq.com>
2017-06-05 16:49:47 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by 香葱 on 2017/6/1.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+          public  String aqi;
+          public  String pm25 ;
+
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/gson/Basic.java
@@ -0,0 +1,19 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by 香葱 on 2017/6/1.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+    public  Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String uodateTime;
+    }
+}
